7ec14b094f9305ca6fe057980d23eaafc14b3591
b0zsz <96700102+b0zsz@users.noreply.github.com>
2022-05-21 09:18:58 +0600

    Add files via upload

---
+++ asg 3/bst.java
@@ -0,0 +1,77 @@
+public class BST <K extends Comparable<K> ,V>{
+    private Node root;
+    private class Node
+    {
+        private K key;
+        private V val;
+        private Node left,right;
+        public Node(K key, V val)
+        {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public void put(K key, V val)
+    {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node root, K key, V val){
+        if(root == null){
+            root = new node(key, val);
+            return root;
+        }
+        if(key < root.key){
+            root.left = put(root.left, key, val);
+        }
+        else if(key > root.key){
+            root.right = put(root.right, key, val);
+        }
+        return root;
+    }
+
+    public V get(K key)
+    {
+        return get(root, key);
+    }
+    private V get(Node root, K key){
+        if(root == null){
+            return null;
+        }
+        if(root.key == key){
+            return root.val;
+        }
+        if(root.key > key){
+            return get(root.left, key);
+        }
+        return get(root.right, key);
+    }
+
+    public void delete(K key)
+    {
+        delete(root, key);
+    }
+    private Node delete(Node root, K key){
+        if(root == null){
+            return null;
+        }
+
+        if(key < root.key){
+            root.left = delete(root.left, key);
+        }
+        else if(key > root.key){
+            root.right = delete(root.right, key);
+        }
+        else{
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+            root.right = delete(root.right, root.key);
+        }
+        return root;
+    }
+}
---
+++ asg 3/myhashtable.java
@@ -0,0 +1,107 @@
+public class MyHashTable <K,V>
+{
+    private class HashNode<K,V>
+    {
+        private K key;
+        private V val;
+        private HashNode<K,V> next;
+        public HashNode(K key, V val){
+            this.key = key;
+            this.val = val;
+        }
+        public String toString(){
+            return "{" + key + " " + val + '}';
+        }
+    }
+    private HashNode<K,V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    public MyHashTable()
+    {
+        this(M);
+    }
+    public MyHashTable(int M)
+    {
+        this.M = size;
+        chainArray = new HashNode[M];
+        this.size = 0;
+    }
+    private int hash(K key)
+    {
+        return key.toString().length() % chainArray.length;
+    }
+
+    public void put(K key, V val)
+    {
+        int hashKey = hash(key);
+        HashNode main = chainArray[hashKey];
+        HashNode newNumb = new HashNode(key,val);
+        newNumb.next = main.next;
+        main.next = newNumb;
+    }
+
+    public V get(K key)
+    {
+        int hashKey = hash(key);
+        HashNode main = chainArray[hashKey];
+        while(main != null)
+        {
+            if(main.key == key)
+            {
+                System.out.println(main.val);
+            }
+            main = main.next;
+        }
+        return null;
+    }
+
+    public V remove(K key)
+    {
+        int hashKey = hash(key);
+        HashNode main = chainArray[hashKey];
+        HashNode last = null;
+        while(main!= null)
+        {
+            if(main.key == key)
+            {
+                break;
+            }
+            last = main;
+            main = main.next;
+        }
+        if(main == null)
+        {
+            return null;
+        }
+        size--;
+        if(last!= null)
+        {
+            last.next = main.next;
+        }
+        else{
+            chainArray[hashKey] = main.next;
+        }
+        return (V) main.val;
+    }
+
+    public boolean contains(V val)
+    {
+        return contains(val);
+    }
+
+    public void getKey(V val)
+    {
+        int hashedVal = hash((K) val);
+        HashNode main = chainArray[hashedVal];
+        while(main != null){
+            if(main.val == val)
+            {
+                System.out.println(main.key);
+            }
+            main = main.next;
+        }
+    }
+}
+
+
